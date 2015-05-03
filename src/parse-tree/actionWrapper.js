a225f8ab2e6888ec30f5afd087b455b36066a68e
Bob Cardenas <bobcardenas@gmail.com>
2015-05-03 11:16:13 -0500

    Ensure route handlers are bound to router instance
    Reviewed by J Husain

--- src/parse-tree/actionWrapper.js
+++ src/parse-tree/actionWrapper.js
@@ -3,7 +3,7 @@ function createNamedVariables(virtualPath, action) {
     // TODO: do this
     return function(matchedPath) {
         var convertedPath = convertPathToVirtual(matchedPath, virtualPath);
-        return action(convertedPath);
+        return action.call(this, convertedPath);
     };
 }
 module.exports = createNamedVariables;
