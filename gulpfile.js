8ad3264256de13bd548901dc60b71f1db7ada678
Laurence Rowe <lrowe@netflix.com>
2016-04-18 10:24:09 -0700

    Upgrade to eslint 2. Use nested .eslintrc for config for better editor integration.

--- gulpfile.js
+++ gulpfile.js
@@ -8,49 +8,21 @@ gulp.task('lint', ['lint-src', 'lint-test', 'lint-root']);
 
 gulp.task('lint-src', function() {
     return gulp.src('src/**/*.js').
-        pipe(eslint({
-            globals: {
-                'require': false,
-                'module': false
-            },
-            reset: true,
-            useEslintrc: true
-        })).
+        pipe(eslint()).
         pipe(eslint.format()).
         pipe(eslint.failAfterError());
 });
 
 gulp.task('lint-test', function() {
     return gulp.src('test/**/*.js').
-        pipe(eslint({
-            globals: {
-                'require': false,
-                'module': false,
-                'it': false,
-                'xit': false,
-                'describe': false
-            },
-            reset: true,
-            rules: {
-                'max-len': [2, 200],
-                'no-unused-expressions': 0
-            },
-            useEslintrc: true
-        })).
+        pipe(eslint()).
         pipe(eslint.format()).
         pipe(eslint.failAfterError());
 });
 
 gulp.task('lint-root', function() {
     return gulp.src('*.js').
-        pipe(eslint({
-            globals: {
-                'require': false,
-                'module': false
-            },
-            reset: true,
-            useEslintrc: true
-        })).
+        pipe(eslint()).
         pipe(eslint.format()).
         pipe(eslint.failAfterError());
 });
